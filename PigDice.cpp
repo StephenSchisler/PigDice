b3c02fa01a0a3e60dfe3cf7ff54df3f3dd0bdceb
Stephen Schisler (StephenSchisler) <schisler@umich.edu>
2026-09-03 17:22:08 -0400

    Test to see if the push function works

--- PigDice.cpp
+++ PigDice.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 // Build your solution starting from this code.
 
@@ -11,16 +13,70 @@ struct GameState {
     bool turn_over = false;
 };
 
-
-void addScore(GameState &gs) {
-    gs.game_score += 10;
-}
+void take_turn(GameState &mg); // prototype statements starts here
+void play_game(GameState &mg); // note: with "&" makes it a reference variable
+void roll(GameState &mg); // reference varibles give an address insted of copys of the variable
+void hold(GameState &mg); // ends here
 
 int main() {
     GameState my_game; // instantiate a GameState object
-    addScore(my_game);
-    std::cout << "Game score is: " << my_game.game_score << std::endl;
-   // display_rules(); // call the display_rules function
-    // play_game(my_game); // call the play_game function and pass the GameState object
+    //display_rules(); // call the display_rules function
+    play_game(my_game); // call the play_game function and pass the GameState object
     return 0;
+}
+
+
+
+void play_game(GameState &mg) {
+    while (!mg.game_over){
+        take_turn(mg); // 1st,  take your the turn
+        mg.game_score += mg.score_this_turn; // alter values to mach the turns result
+        if (mg.game_score >= 20) { // check if the goal score has been reached
+            mg.game_over = true;
+        }
+        else {
+            mg.turn_over = false;
+            mg.score_this_turn = 0;
+        }
+    }
+    std::cout << "You finished wih a final score of " << mg.game_score << " in " << mg.turn_count << " turns!" << std::endl;
+    std::cout << "\nThanks for playing PIG Dice!" << std::endl;
+}
+void take_turn(GameState &mg) {
+    mg.turn_count++; // means the same thing as mg.turn_count = mg.turn_count + 1
+    std::cout << "TURN " << mg.turn_count << " - Game Score: " << mg.game_score << std::endl;
+    while (!mg.turn_over) {
+        std::cout << "\nroll or hold? (r/h): " << std::endl;
+        std::cin >> mg.choice;
+        if (mg.choice == 'r') {
+            roll(mg);
+        }
+        else if (mg.choice == 'h') {
+            hold(mg);
+        }
+        else {
+            std::cout << "Invalid choice!"<< std::endl;
+        }
+
+    }
+    std::cout << "\nScore Banked This Turn: " << mg.score_this_turn << std::endl;
+}
+
+void roll(GameState &mg) {
+    srand(time(NULL));
+    int die = rand() % 6 + 1;
+    std::cout << "Die: " << die;
+    if (die == 1) {
+        std::cout << "\nTurn over. No score.";
+        mg.score_this_turn = 0;
+        mg.turn_over = true;
+    }
+    else {
+        mg.score_this_turn += die;
+        std::cout << " - Running score this turn: " << mg.score_this_turn;
+    }
+}
+
+void hold(GameState &mg) {
+    mg.turn_over = true;
 }
